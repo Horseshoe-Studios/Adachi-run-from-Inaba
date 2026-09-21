7c9795f62b58b20a73f2de91c3bd8191281a7ab3
decarlos78 <decarlos782007@gmail.com>
2026-09-21 14:01:56 +0200

    Todo hecho de mecánicas e interfaces, falta ajustar cosas y retocar las mecánicas, además de acabar el escenario

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Restart_Level()
+    {
+        Debug.Log("Poner la misma escena");
+    }
+    public void Main_Menu()
+    {
+        Debug.Log("Ir al menú");
+    }
+}
